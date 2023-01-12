888e7b4405c58d90f316c7888f677e7396afa53d
ke444a <kechaofficial@gmail.com>
2023-01-12 19:35:30 +0000

    Adjusted CORS policy for backend server

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,12 @@ require('dotenv').config({ path: '../.env' });
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use('/api', require('./routes'));
-
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://weather-application-rltf.onrender.com'],
+    origin: '*',
     methods: ['GET']
 }
 app.use(cors(corsOptions));
+
+app.use('/api', require('./routes'));
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
